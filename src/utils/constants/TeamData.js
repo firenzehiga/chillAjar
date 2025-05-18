983b2952a9fc9d0e8a2cc728de119065736c45bc
Firenze Higa Putra <firenzehiga@gmail.com>
2025-05-18 11:17:15 +0700

    update

--- src/utils/constants/TeamData.js
+++ src/utils/constants/TeamData.js
@@ -7,14 +7,12 @@ const teamsData = [
 	{
 		name: "Farrel Zulviano",
 		role: "Product Manager",
-		image:
-			"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&q=80&w=200",
+		image: eko,
 	},
 	{
 		name: "Arby Ali Amludin",
 		role: "Scrum Master",
-		image:
-			"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&q=80&w=200",
+		image: eko,
 	},
 	{
 		name: "Ahmad Faiz Al Asad",
