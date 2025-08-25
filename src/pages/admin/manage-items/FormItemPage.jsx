737814fe7cab126f2a8b28cfe201b3157ea3a473
Firenze Higa Putra <firenzehiga@gmail.com>
2025-08-25 18:14:19 +0700

    feat: Add Loader2 icon to FormItemPage for improved loading indication

--- src/pages/admin/manage-items/FormItemPage.jsx
+++ src/pages/admin/manage-items/FormItemPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../../../api.jsx";
-import { Package, ArrowLeft, AlertCircle } from "lucide-react";
+import { Package, ArrowLeft, AlertCircle, Loader2 } from "lucide-react";
 import Swal from "sweetalert2";
 import { FormSkeletonCard } from "../../../components/Skeleton/FormSkeletonCard";
 import { useQueryClient } from "@tanstack/react-query";
