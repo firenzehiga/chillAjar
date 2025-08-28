a818e65a128930fcaab9fe0e10e718df0ce6a541
Firenze Higa Putra <firenzehiga@gmail.com>
2025-08-28 15:24:12 +0700

    feat: Add package details display in AdminPaymentsPage, AdminSessionsPage, and MentorSchedulePage

--- src/pages/admin/manage-payments/AdminPaymentsPage.jsx
+++ src/pages/admin/manage-payments/AdminPaymentsPage.jsx
@@ -658,7 +658,7 @@ export function AdminPaymentsPage() {
 										<span className="w-48 font-medium text-gray-900">
 											Paket Belajar:
 										</span>
-										<span className="capitalize">
+										<span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-yellow-600/20 ring-inset">
 											{data.paket?.nama || "-"}
 										</span>
 									</p>
