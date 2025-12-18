4453c2d13b1a9c289bae18e21faa10e5568fd44b
Firenze Higa Putra <firenzehiga@gmail.com>
2025-12-18 09:13:07 +0700

    fix: update favicon and document title formatting for consistency

--- src/hooks/utils/useDocumentTitle.js
+++ src/hooks/utils/useDocumentTitle.js
@@ -4,15 +4,15 @@ export const useDocumentTitle = (title, subtitle) => {
 	useEffect(() => {
 		const newTitle =
 			title && subtitle
-				? `${title} | ${subtitle}`
+				? `${title} - ${subtitle}`
 				: title
-				? ` ChillAjar | ${title}`
-				: "ChillAjar | Peer-to-Peer Tutoring Platform";
+				? ` ChillAjar - ${title}`
+				: "ChillAjar - Peer-to-Peer Tutoring Platform";
 		document.title = newTitle;
 
 		// Cleanup: reset ke default saat unmount
 		return () => {
-			document.title = "ChillAjar | Peer-to-Peer Tutoring Platform";
+			document.title = "ChillAjar - Peer-to-Peer Tutoring Platform";
 		};
 	}, [title]);
 };
